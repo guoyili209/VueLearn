1ccee566e443519498373ed690b6aa3d8fa838b9
guoyili209 <guoyili209@gmail.com>
2020-02-21 17:24:31 +0800

    update vuex

--- Vue-CLItest/cli2/vuextest/src/components/HelloWorld.vue
+++ Vue-CLItest/cli2/vuextest/src/components/HelloWorld.vue
@@ -1,44 +1,54 @@
 <template>
   <div class="hello">
-    <h2>{{ $store.state.conter }}</h2>
+    <h2>{{ $store.getters.counter }}</h2>
     <button @click="add">+</button>
-      <button @click="sub">-</button>
+    <button @click="sub">-</button>
+    <button @click="addCount(5)">+5</button>
+    <h2>{{ $store.getters.getName }}</h2>
   </div>
 </template>
 
 <script>
+import {INCREMENT} from '../store/mutations-types'
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  name: "HelloWorld",
+  created(){
+    this.$store.dispatch('updateInfo').then((res)=>{
+      console.log(res);
+    })
+    this.$store.dispatch('modifyName','多肉葡萄').then((res)=>{
+      console.log(res);
+    })
   },
-  methods:{
-    add(){
-      this.$store.commit('increment');
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    add() {
+      this.$store.commit(INCREMENT);
     },
-    sub(){
-      this.$store.commit('decrement');
+    sub() {
+      this.$store.commit("decrement");
+    },
+    addCount(count) {
+      //普通提交风格
+      this.$store.commit("addCount", count /*payload负载(专业名称)*/);
+      //对象提交风格
+      // this.$store.commit({
+      //   type: "increment",
+      //   count: 10
+      // });
+      //响应式增删属性
+      // Vue.set(obj,'xx',1);
+      // Vue.delete(obj,'xx');
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- Vue-CLItest/cli2/vuextest/src/store/index.js
+++ Vue-CLItest/cli2/vuextest/src/store/index.js
@@ -1,24 +1,87 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {INCREMENT} from './mutations-types'
 
 Vue.use(Vuex)
 
+const moduleA={
+    state:{
+        name:'xxx'
+    },
+    getters:{
+        getName(state){
+            return state.name;
+        }
+    },
+    mutations:{
+        setName(state,payload){
+            state.name=payload;
+        }
+    },
+    actions: {
+        // 对于模块内部的 action，局部状态通过 context.state 暴露出来，根节点状态则为 context.rootState
+        modifyName ({ state, commit, rootState },payload) {
+            return new Promise((resovle,reject)=>{
+                setTimeout(()=>{
+                    commit('setName',payload);
+                    resovle('名字修改成功');
+                },3000)
+                
+            })
+        }
+      }
+}
+const moduleB={
+    state:{
+        name:'yyy'
+    }
+}
+
 const store = new Vuex.Store({
+    //相当于变量
     state:{
         conter:1000
     },
+    //同步方法
     mutations:{
         //方法
-        increment(state){
+        [INCREMENT](state){
             state.conter++;
         },
         decrement(state){
             state.conter--;
+        },
+        addCount(state,payload){
+            state.conter+=payload;
+            // state.conter+=payload.count;
         }
     },
-    actions:{},
-    getters:{},
-    modules:{}
+    //异步方法
+    actions:{
+        updateInfo(context,payload){
+            return new Promise((resovle,reject)=>{
+                setTimeout(()=>{
+                    context.commit('addCount',100);
+                    resovle('执行成功');
+                },2000)
+                
+            })
+        }
+    },
+    //相当于计算属性compute
+    getters:{
+        counter(state){
+            return state.conter;
+        },
+        //注意，getter 在通过方法访问时，每次都会去进行调用，而不会缓存结果。
+        // getTodoById: (state) => (id) => {
+        //     return state.todos.find(todo => todo.id === id)
+        // }
+    },
+    modules:{
+        a:moduleA,
+        b:moduleB
+    }
 })
 
 export default store;
